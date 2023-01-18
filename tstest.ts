487340f340d66f60c231e6ad23180fca80cdeb41
sauravsharma2025 <sauravsharma2025@gmail.com>
2023-01-19 00:45:59 +0530

    this is basic node js import export

---
+++ tstest.ts
@@ -0,0 +1,16 @@
+let a1 = 10;
+a1 = 20;
+/* a1 = "Rahul";
+a1 = true;
+a1 = "tarun";
+a1 = [1, 2, 3, 45];
+a1 = null;
+a1 = { name: "Ankit" }; */
+
+console.log(a1);
+
+function addNumsV1(a: number, b: number): number {
+  return a + b;
+}
+
+const results = addNumsV1(10, 12);
